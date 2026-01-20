f03830405b3b89ad2f17bd0f741c4a1271f7af5b
Hanna Böös <ens21hbs@cs.umu.se>
2026-01-20 15:18:04 +0100

    inital code - drag and drop

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, intial-scale=1.0"/>
+		<title>Calender Project</title>
+
+		<meta name="description" content="My Own Calender Project">
+
+		<link rel="stylesheet" href="style.css"/>
+	</head>
+
+	<body>
+
+		<header>
+			<h1>My Calendar Project</h1>
+		</header>
+
+		<div class="container">
+			<div id="left">
+				<div class="list" draggable="true"><img src="assets/images/icon.png">List Item 1</div>
+				<div class="list" draggable="true"><img src="assets/images/icon.png">List Item 2</div>
+				<div class="list" draggable="true"><img src="assets/images/icon.png">List Item 3</div>
+				<div class="list" draggable="true"><img src="assets/images/icon.png">List Item 4</div>
+			</div>
+			<div id="right"></div>
+		</div>
+
+		<script src="assets/scripts/calender.js"></script>
+
+	</body>
+
+</html>
+
+
